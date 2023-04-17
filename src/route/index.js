ecf5198c3fbd1175655f76ae45d608097783d21c
1wayowl <l0belov0l@gmail.com>
2023-04-18 01:37:23 +0300

    end task31

--- src/route/index.js
+++ src/route/index.js
@@ -784,7 +784,7 @@ router.get('/task21', function (req, res) {
 
   //            ↙ cюди вводимо назву файлу з сontainer
   res.render('task21', {
-layout: 'basic',
+    layout: 'basic',
 
     heading: {
       main: {
@@ -992,7 +992,7 @@ router.get('/task31', function (req, res) {
           description:
             "Tokyo, Japan’s bustling capital, mixes the ultramodern and the traditional, from neon-lit skyscrapers to historic temples. The opulent Meiji Shinto Shrine is known for its towering gate and surrounding woods. The Imperial Palace sits amid large public gardens. The city's many museums offer exhibits ranging from classical art (in the Tokyo National Museum) to a reconstructed kabuki theater (in the Edo-Tokyo Museum).",
           button: {
-            text: 'Reac',
+            text: 'Read',
             href: 'https://github.com/',
           },
         },
@@ -1005,7 +1005,7 @@ router.get('/task31', function (req, res) {
           description:
             "Sydney, capital of New South Wales and one of Australia's largest cities, is best known for its harborfront Opera House, with a distinctive sail-like design. Massive Darling Harbour and the smaller Circular Quay port are hubs of waterside life, with the arched Harbour Bridge and esteemed Royal Botanic Garden nearby. Sydney Tower’s outdoor platform, the Skywalk, offers 360-degree views of the city and suburbs.",
           button: {
-            text: 'Reac',
+            text: 'Read',
             href: 'https://github.com/',
           },
         },
@@ -1174,7 +1174,6 @@ router.get('/task31', function (req, res) {
 })
 
 // ================================================================
-
 
 // Підключаємо роутер до бек-енду
 module.exports = router
